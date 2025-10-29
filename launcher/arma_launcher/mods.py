52d89adeee2818a72639f518f59e04f9c92dde6b
Don <github2@chwolf.net>
2025-10-29 23:22:20 +0100

    added ocap

--- launcher/arma_launcher/mods.py
+++ launcher/arma_launcher/mods.py
@@ -161,7 +161,8 @@ class ModManager:
             srv_path=self.cfg.this_server_mods / name
             mod_path=self.cfg.common_share / name
             if not os.path.exists(srv_path):
-                shutil.copytree(str(mod_path), str(srv_path), dirs_exist_ok=True)
+                logger.debug(f"{mod_path} -> {srv_path}")
+                shutil.copytree(mod_path, srv_path, dirs_exist_ok=True)
             self._normalize_mod_case(mod_path)
             self._copy_keys(mod_path, name, "0")
             dst = self.servermods_dir / f"@{name}"
